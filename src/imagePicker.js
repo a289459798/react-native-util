821acb6f609e8200d7a4c9a674afdb831ddd60ab
zhangzy <zhangzy0816@gmail.com>
2020-02-19 12:32:57 +0800

    [新增功能](1.0.0): android 保存图片优化

--- src/imagePicker.js
+++ src/imagePicker.js
@@ -1,6 +1,7 @@
-import {NativeModules, Platform} from 'react-native';
-import CameraRoll from "@react-native-community/cameraroll";
+import {NativeModules, Platform, PermissionsAndroid} from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
 import file from './file';
+import RNFS from 'react-native-fs';
 
 const {RNICImagePicker} = NativeModules;
 
@@ -16,17 +17,53 @@ class ICImagePicker {
             return CameraRoll.saveToCameraRoll(url, 'photo');
         } else {
             return new Promise((callback, errorCallback) => {
-                file.download('share', 'share.jpg', data.image).then((res) => {
-                    // CameraRoll.saveToCameraRoll('file://' + RNFS.CachesDirectoryPath + '/com.ichong.CloudMarket/share/store.jpg').then(() => {
-                    //     callback();
-                    // }, error => {
-                    //     errorCallback(error);
-                    // });
-                }, (error) => {
-                    errorCallback(error);
+                PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE).then((res) => {
+                    if (!res) {
+                        PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+                            {
+                                title: '申请写入权限',
+                                message:
+                                    '需要保存图片到相册',
+                                buttonNegative: '残忍拒绝',
+                                buttonPositive: '同意',
+                            }).then((granted) => {
+                            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                                this._saveWithAndroid(url, callback, errorCallback);
+                            } else {
+                                errorCallback('用户拒绝授权');
+                            }
+                        });
+                    } else {
+                        this._saveWithAndroid(url, callback, errorCallback);
+                    }
                 });
             });
         }
+    }
+
+    _saveWithAndroid(url, callback, errorCallback) {
+        if (url.indexOf('http') == 0) {
+            file.download('share', 'share.png', url).then((res) => {
+                CameraRoll.saveToCameraRoll('file://' + RNFS.ExternalDirectoryPath + '/cache/share/share.png').then(() => {
+                    callback();
+                }, error => {
+                    errorCallback(error);
+                });
+            }, (error) => {
+                errorCallback(error);
+            });
+        } else {
+            file.write('share', 'share.png', url, 'base64').then((res) => {
+                CameraRoll.saveToCameraRoll('file://' + RNFS.ExternalDirectoryPath + '/cache/share/share.png').then(() => {
+                    callback();
+                }, error => {
+                    errorCallback(error);
+                });
+            }, (error) => {
+                errorCallback(error);
+            });
+        }
+
     }
 }
 
