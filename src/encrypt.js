c66a18c593a7d82ba7e8b46d8c4e6c122ddf5ba6
jszh <969883825@qq.com>
2020-09-01 13:35:24 +0800

    [Bug修复](1.0.0): 二维码扫描结果问题
    二维码扫描结果问题

--- src/encrypt.js
+++ src/encrypt.js
@@ -5,33 +5,26 @@ const {RNEncrypt} = NativeModules;
 class EncryptManage {
 
     string2GBK(str, cb) {
-
-        Encryption.string2GBK(str, (data) => {
+        RNEncrypt.string2GBK(str, (data) => {
             cb && cb(data);
         });
     }
 
     encode(str, key) {
-
         return new Promise(function (callback, errorCallback) {
-            RNEncrypt.encode(str, key)
-                .then((data) => {
-
-                    callback(data);
-                });
+            RNEncrypt.encode(str, key).then((data) => {
+                callback(data);
+            });
         });
     }
 
     decode(str, key) {
-
         return new Promise(function (callback, errorCallback) {
-            RNEncrypt.decode(str, key)
-                .then((data) => {
-                    callback(data);
-                }, (error) => {
-
-                    errorCallback(error);
-                });
+            RNEncrypt.decode(str, key).then((data) => {
+                callback(data);
+            }, (error) => {
+                errorCallback(error);
+            });
         });
     }
 }
