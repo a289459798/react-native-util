ae198360f68f4b1cbaef0d47a94e542b2b381cb9
zhangzy <zhangzy0816@gmail.com>
2021-05-15 14:34:45 +0800

    [新增功能](1.0.0): 修改android 提醒

--- android/src/main/java/com/ichong/zzy/util/receiver/NotifyReceiver.java
+++ android/src/main/java/com/ichong/zzy/util/receiver/NotifyReceiver.java
@@ -1,13 +1,13 @@
 package com.ichong.zzy.util.receiver;
 
-import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+import android.annotation.SuppressLint;
+import android.app.*;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import androidx.core.app.NotificationCompat;
+import com.ichong.zzy.util.R;
 
 /**
  * Created by zzy on 2019/3/27.
@@ -26,34 +26,37 @@ public class NotifyReceiver extends BroadcastReceiver {
 
         String msg = intent.getStringExtra("title");
 
-        Intent it = new Intent(context, activity.getClass());
-
-        //创建一个启动其他Activity的Intent
-        PendingIntent pi = PendingIntent.getActivity(context
-            , 0, it, 0);
-
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, "notify");
         int id = activity.getResources().getIdentifier("ic_launcher", "mipmap", activity.getPackageName());
 
-        //设置标题
-        mBuilder.setContentTitle("消息提醒")
-            //设置内容
-            .setContentText(msg)
-            //设置小图标
-            .setSmallIcon(id)
-            //设置通知时间
-            .setWhen(System.currentTimeMillis())
-            //首次进入时显示效果
-            .setTicker(msg)
-            //设置通知方式，声音，震动，呼吸灯等效果，这里通知方式为声音
-            .setDefaults(Notification.DEFAULT_SOUND)
-            .setContentIntent(pi);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
+            PendingIntent.FLAG_ONE_SHOT);
+        final NotificationManager notificationManager = (NotificationManager) this.activity.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            @SuppressLint("WrongConstant")
+            NotificationChannel notificationChannel = new NotificationChannel("ID", "NAME", importance);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setShowBadge(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(notificationChannel);
+            }
+        }
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, "ID");
+        mBuilder.setSmallIcon(id);
+        mBuilder.setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        mBuilder.setDefaults(Notification.DEFAULT_ALL);
+        mBuilder.setAutoCancel(true);
+        mBuilder.setContentTitle("预约提醒");
+        mBuilder.setContentText(msg);
+        mBuilder.setContentIntent(pendingIntent);
+        final Notification notify = mBuilder.build();
 
-        //获取系统的NotificationManager服务
-        NotificationManager notificationManager = (NotificationManager)
-            context.getSystemService(Context.NOTIFICATION_SERVICE);
-        //发送通知
-        notificationManager.notify(1, mBuilder.build());
+        //随机id 1000-2000
+        final int notifyId = (int) (Math.random() * 1000 + 1000);
+        if (notificationManager != null) {
+            notificationManager.notify(notifyId, notify);
+        }
 
     }
 }
