2517993b7126316f6d3c90cd8633e2a7044ade1e
zhangzy <289459798@qq.com>
2025-07-31 15:02:05 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,19 +1,13 @@
 import QiNiu from './src/qiniu';
 import Encrpyt from './src/encrypt';
-import Remind from './src/remind';
-import Event from './src/event';
 import Map from './src/map';
-import ICImagePicker from './src/imagePicker';
 import ICFile from './src/file';
 import ImageBrowser from './src/imageBrowser';
 
 export {
     QiNiu,
     Encrpyt,
-    Remind,
-    Event,
     Map,
-    ICImagePicker,
     ICFile,
     ImageBrowser,
 };
